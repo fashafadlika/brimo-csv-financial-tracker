c07c752ca86163d948a3d713163a7770ecfd9ec8
znva <fashafadlika@gmail.com>
2026-03-03 03:39:10 +0700

    Update grafik pengeluaran & pemasukan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import json
 
 #baca csv (masukan file csv)
 def load_data(file_path):
@@ -10,6 +11,27 @@ def load_data(file_path):
     data["MUTASI_KREDIT"] = pd.to_numeric(data["MUTASI_KREDIT"], errors="coerce")
     data["TGL_TRAN"] = pd.to_datetime(data["TGL_TRAN"])
 
+    return data
+
+def load_rules(file_path):
+    with open(file_path, "r") as file:
+        rules = json.load(file)
+    return rules
+
+def kategori_pengeluaran(deskripsi, rules):
+    deskripsi = str(deskripsi).upper()
+
+    for kategori, keywords in rules.items():
+        for keyword in keywords:
+            if keyword in deskripsi:
+                return kategori
+            
+    return "Lainnya"
+
+def proses_data(data, rules):
+    data["KATEGORI"] = data["REMARK_CUSTOM"].apply(
+        lambda x: kategori_pengeluaran(x, rules)
+    )
     return data
 
 def hitung_cashflow_perhari(data):
@@ -26,6 +48,7 @@ def hitung_ringkasan(data):
 
     return total_masuk, total_keluar, selisih
 
+
 def plot_pengeluaran_pemasukan(perhari, total_masuk, total_keluar, selisih):
     #Subplot 1: Pengeluaran
     plt.subplot(2, 1, 1)
@@ -33,21 +56,30 @@ def plot_pengeluaran_pemasukan(perhari, total_masuk, total_keluar, selisih):
     plt.title("Pengeluaran per Hari")
     plt.xticks(rotation = 45)
 
+    total_keluar = (
+        f"Total Keluar: Rp. {total_keluar:,}\n"
+    )
+
+    plt.text(
+        0.02, 0.95,
+        total_keluar,
+        transform=plt.gca().transAxes,
+        verticalalignment="top"
+    )
+
     #Subplot 2: Pemasukan
     plt.subplot(2, 1, 2)
     plt.plot(perhari.index, perhari["MUTASI_KREDIT"])
     plt.title("Pemasukan per Hari")
     plt.xticks(rotation = 45)
 
-    total_masuk_keluar = (
+    total_masuk = (
         f"Total Masuk: Rp. {total_masuk:,}\n"
-        f"Total Keluar: Rp. {total_keluar:,}\n"
-        f"Selisih: Rp. {selisih}"
     )
 
     plt.text(
         0.02, 0.95,
-        total_masuk_keluar,
+        total_masuk,
         transform=plt.gca().transAxes,
         verticalalignment="top"
     )
@@ -57,7 +89,11 @@ def plot_pengeluaran_pemasukan(perhari, total_masuk, total_keluar, selisih):
 
 # Main Program
 data = load_data("desember2025.csv")
+rules = load_rules("rules.json")
+data = proses_data(data, rules)
 cashflow_perhari = hitung_cashflow_perhari(data)
 total_masuk, total_keluar, selisih = hitung_ringkasan(data)
 plot_pengeluaran_pemasukan(cashflow_perhari, total_masuk, total_keluar, selisih)
+
+
 
